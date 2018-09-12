3ed3782dce9418afef368ee947d8d597abf8eea8
MaximilianoRolon <maxirolon@gmail.com>
2018-09-12 17:44:51 -0300

    Final version

--- app/src/main/java/com/example/maxir/menuproyect/MainActivity.java
+++ app/src/main/java/com/example/maxir/menuproyect/MainActivity.java
@@ -1,13 +1,112 @@
 package com.example.maxir.menuproyect;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.PopupMenu;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvNombre;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvNombre = (TextView) findViewById(R.id.tvNombre);
+        registerForContextMenu(tvNombre);
+
+        tvNombre.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                tvNombre.showContextMenu();
+                return true;
+            }
+        });
+
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_opciones, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+
+            case R.id.mAbout:
+                Intent intent = new Intent(this, ActivityAbout.class);
+                startActivity(intent);
+                break;
+
+            case R.id.mSettings:
+                Intent i = new Intent(this, ActivitySettings.class);
+                startActivity(i);
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        getMenuInflater().inflate(R.menu.menu_contexto, menu);
+        //MenuInflater inflater = new MenuInflater(this);
+
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+
+            case R.id.mEdit:
+                Toast.makeText(this, getResources().getString(R.string.edit_title), Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.mDelete:
+                Toast.makeText(this, getResources().getString(R.string.delete_title), Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onContextItemSelected(item);
+    }
+
+    public void levantarMenuPopUp(View v){
+        TextView tvPopup = (TextView) findViewById(R.id.tvPopup);
+        PopupMenu popupMenu = new PopupMenu(this, tvPopup);
+        popupMenu.getMenuInflater().inflate(R.menu.menu_popup, popupMenu.getMenu());
+
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()){
+
+                    case R.id.mView:
+                        Toast.makeText(getBaseContext(), getResources().getString(R.string.menu_view), Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.mViewDetail:
+                        Toast.makeText(getBaseContext(), getResources().getString(R.string.menu_view_detail), Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return true;
+            }
+        });
+
+        popupMenu.show();
+    }
+
 }
